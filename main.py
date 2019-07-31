399456ab28beecb7094ed6e85df56aa6052a65f0
abrie <abrie@example.com>
2019-07-31 17:50:13 -0400

    Graph original, mask, and gradient.

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ import matplotlib.pyplot as plt
 import fitter
 import loader
 
-def show_slices(slices, gradients):
-    fig, axes = plt.subplots(2, len(slices), sharey=True, sharex=True)
+def show_slices(slices, masks, gradients):
+    fig, axes = plt.subplots(3, len(slices), sharey=True, sharex=True)
     for i, s in enumerate(slices):
         axes[0][i].imshow(s, cmap="gray", origin="lower")
 
+    for i, s in enumerate(masks):
+        axes[1][i].imshow(s, cmap="gray", origin="lower")
+
     for i, s in enumerate(gradients):
-        axes[1][i].imshow(s, cmap=plt.cm.jet, origin="lower")
+        axes[2][i].imshow(s, cmap=plt.cm.jet, origin="lower")
 
-#show_slices(list(map(lambda image: image["data"][35,:,:], images)))
+images = loader.loadImages()
+mask = loader.loadMask()
 
-datalist = list(map(lambda image: image["data"], images))
-gradients = fitter.compute_gradients(datalist)
+raw = list(map(lambda i: i["data"], images))
+masked = list(map(lambda i: fitter.apply_mask(i, mask, 95.0), raw))
+grads = fitter.compute_gradients(masked)
+masked_grads = list(map(lambda i: fitter.apply_mask(i, mask, 95.0), grads))
 
 slicehere = 30
-ss = list(map(lambda image: image["data"][slicehere,:,:], images))
-gs = list(map(lambda image: image[slicehere,:,:], gradients))
-show_slices(ss, gs)
+ss = list(map(lambda image: image[slicehere,:,:], raw))
+ms = list(map(lambda image: image[slicehere,:,:], masked))
+gs = list(map(lambda image: image[slicehere,:,:], masked_grads))
+show_slices(ss, ms, gs)
 
 plt.show()
