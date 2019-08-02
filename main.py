ad82548eea4580cbc6bced70276f7969cadd2e92
abrie <abrie@users.noreply.github.com>
2019-08-02 09:12:25 -0400

    Rename scaled 'data' variable to 'scaled'.

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ data = dataset["data"]
 mask = dataset["mask"]
 threshold = 95.0
 
-data = fitter.to_hounsfield_units(data)
-masked = list(map(lambda i: fitter.apply_mask(i, mask, threshold), data))
+scaled = fitter.to_hounsfield_units(data)
+masked = list(map(lambda i: fitter.apply_mask(i, mask, threshold), scaled))
 grads = fitter.compute_gradients(masked)
 masked_grads = list(map(lambda i: fitter.apply_mask(i, mask, threshold), grads))
 
 slicehere = 30
-data_slices = list(map(lambda image: image[slicehere,:,:], data))
+scaled_slices = list(map(lambda image: image[slicehere,:,:], scaled))
 masked_slices = list(map(lambda image: image[slicehere,:,:], masked))
 gradient_slices = list(map(lambda image: image[slicehere,:,:], masked_grads))
 
-plotter.plot(data_slices, masked_slices, gradient_slices)
+plotter.plot(scaled_slices, masked_slices, gradient_slices)
 plotter.show()
