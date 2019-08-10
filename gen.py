4206ed72ec2e75f263b44ff3d840339f8a6b1f27
abrie <abrie@users.noreply.github.com>
2019-08-10 16:43:27 -0400

    Generate test files to study gradient behavior.

---
+++ gen.py
@@ -0,0 +1,17 @@
+import os
+import math
+import numpy as np
+import builder
+
+def generate_testfiles(path, shape, count):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    for x in range(1, count+1):
+        named = f"CBCT_{x}.nii"
+        data = np.random.choice([math.sin( (2*math.pi/3)*x-1)*100], shape)
+        builder.write_nifti(path, named, data)
+
+    builder.generate_nifti_file(path, "mask.nii", shape, 95)
+
+generate_testfiles("input", (64, 64, 64), 7)
